e6c426557f951b96a3fc1b9f80e42c684e751b26
ambition85 <ambition9385@gmail.com>
2023-03-01 23:44:33 -0500

    Test autocomplete text box

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import { useState } from "react";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//     return (
+//         <div className="container">
+//             <div className="row">
+//                 <header className="header mt-5 mb-4">
+//                     <h1 className="header__title">Autocomplete</h1>
+//                     <p className="header__item">
+//                         <a href="https://github.com/Aveek-Saha/autocomplete">
+//                             <i
+//                                 className="bi-github me-2"
+//                                 role="img"
+//                                 aria-label="GitHub"
+//                             />
+//                             GitHub
+//                         </a>
+//                     </p>
+//                 </header>
+//             </div>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+//             <div class="row">
+//                 <div class="mb-4">
+//                     <textarea
+//                         type="text"
+//                         className="form-control search"
+//                         id="search"
+//                         placeholder="Start typing to see autocomplete suggestions"
+//                     />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
+import emoji from "@jukben/emoji-search";
+
+import "./App.css";
+import "@webscopeio/react-textarea-autocomplete/style.css";
+
+const Item = ({ entity: { name, char } }) => <div>{`${name}: ${char}`}</div>;
+const Loading = ({ data }) => <div>Loading</div>;
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <ReactTextareaAutocomplete
+                    className="my-textarea"
+                    loadingComponent={Loading}
+                    style={{
+                        fontSize: "18px",
+                        lineHeight: "20px",
+                        padding: 5,
+                    }}
+                    ref={(rta) => {
+                        this.rta = rta;
+                    }}
+                    innerRef={(textarea) => {
+                        this.textarea = textarea;
+                    }}
+                    containerStyle={{
+                        marginTop: 20,
+                        width: 400,
+                        height: 100,
+                        margin: "20px auto",
+                    }}
+                    minChar={0}
+                    trigger={{
+                        ":": {
+                            dataProvider: (token) => {
+                                return emoji(token)
+                                    .slice(0, 10)
+                                    .map(({ name, char }) => ({ name, char }));
+                            },
+                            component: Item,
+                            output: (item, trigger) => item.char,
+                        }
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "bootswatch/dist/darkly/bootstrap.min.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
