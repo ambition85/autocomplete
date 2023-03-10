78587ecbd3eb2192cbafe8ae1834a6f0b261b870
ambition85 <ambition9385@gmail.com>
2023-03-10 00:45:24 -0500

    Build autocomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,78 @@
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//     return (
-//         <div className="container">
-//             <div className="row">
-//                 <header className="header mt-5 mb-4">
-//                     <h1 className="header__title">Autocomplete</h1>
-//                     <p className="header__item">
-//                         <a href="https://github.com/Aveek-Saha/autocomplete">
-//                             <i
-//                                 className="bi-github me-2"
-//                                 role="img"
-//                                 aria-label="GitHub"
-//                             />
-//                             GitHub
-//                         </a>
-//                     </p>
-//                 </header>
-//             </div>
-
-//             <div class="row">
-//                 <div class="mb-4">
-//                     <textarea
-//                         type="text"
-//                         className="form-control search"
-//                         id="search"
-//                         placeholder="Start typing to see autocomplete suggestions"
-//                     />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default App;
-
 import React, { Component } from "react";
 import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
-import emoji from "@jukben/emoji-search";
+import { Trie } from "../public/trie";
+import json from "../public/trie.json";
 
 import "./App.css";
 import "@webscopeio/react-textarea-autocomplete/style.css";
 
-const Item = ({ entity: { name, char } }) => <div>{`${name}: ${char}`}</div>;
+const Item = ({ entity: { name, char } }) => <div>{`${name}`}</div>;
 const Loading = ({ data }) => <div>Loading</div>;
+
+// const json = JSON.parse(jsonFile)
+let trie = Trie.from(json);
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-
-                <ReactTextareaAutocomplete
-                    className="my-textarea"
-                    loadingComponent={Loading}
-                    style={{
-                        fontSize: "18px",
-                        lineHeight: "20px",
-                        padding: 5,
-                    }}
-                    ref={(rta) => {
-                        this.rta = rta;
-                    }}
-                    innerRef={(textarea) => {
-                        this.textarea = textarea;
-                    }}
-                    containerStyle={{
-                        marginTop: 20,
-                        width: 400,
-                        height: 100,
-                        margin: "20px auto",
-                    }}
-                    minChar={0}
-                    trigger={{
-                        ":": {
-                            dataProvider: (token) => {
-                                return emoji(token)
-                                    .slice(0, 10)
-                                    .map(({ name, char }) => ({ name, char }));
+            <div className="container">
+                <div className="row">
+                    <header className="header mt-5 mb-4">
+                        <h1 className="header__title">Autocomplete</h1>
+                        <p className="header__item">
+                            <a href="https://github.com/Aveek-Saha/autocomplete">
+                                <i
+                                    className="bi-github me-2"
+                                    role="img"
+                                    aria-label="GitHub"
+                                />
+                                GitHub
+                            </a>
+                        </p>
+                    </header>
+                </div>
+    
+                <div className="row">
+                    <div className="mb-4">
+                        
+                    <ReactTextareaAutocomplete
+                        className="form-control search"
+                        loadingComponent={Loading}
+                        style={{
+                            // fontSize: "18px",
+                            // lineHeight: "20px",
+                            // padding: 5,
+                        }}
+                        ref={(rta) => {
+                            this.rta = rta;
+                        }}
+                        innerRef={(textarea) => {
+                            this.textarea = textarea;
+                        }}
+                        containerStyle={{
+                            height: "75vh",
+                        }}
+                        dropdownStyle={{
+                            // backgroundColor: "#3b3b3b",
+                            color: "black"
+                        }}
+                        minChar={3}
+                        trigger={{
+                            " ": {
+                                dataProvider: (token) => {
+                                    console.log(token);
+                                    return trie
+                                        .suggest(token)
+                                        .slice(0, 10)
+                                        .map(({ name, char }) => ({ name, char }));
+                                },
+                                component: Item,
+                                output: (item, trigger) => " " + item.char,
                             },
-                            component: Item,
-                            output: (item, trigger) => item.char,
-                        }
-                    }}
-                />
+                        }}
+                    />
+                    </div>
+                </div>
             </div>
         );
     }
