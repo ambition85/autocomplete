950f2d76c8fd18b5167dde8eb6c6b5d638c7c649
ambition85 <ambition9385@gmail.com>
2023-06-01 11:33:12 -0400

    Fix stat

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -11,7 +11,7 @@ export default function Stats() {
         <div className="card text-bg-dark mb-3">
             <h4 className="card-header">Search stats</h4>
             <div className="card-body">
-                <h6 className="card-title">Total words: 100k+</h6>
+                <h6 className="card-title">Total words: 125k+</h6>
                 <h6 className="card-title">Matches found: {numResults}</h6>
                 <h6 className="card-title">Time taken: {searchTime}</h6>
             </div>
